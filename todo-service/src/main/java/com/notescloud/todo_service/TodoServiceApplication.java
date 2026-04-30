bc18f97214c677dbd97c66860b0ab2922a2d861e
Mihael Mihaylov <mishoni04@abv.bg>
2026-04-30 23:06:20 +0300

    runnable

--- todo-service/src/main/java/com/notescloud/todo_service/TodoServiceApplication.java
+++ todo-service/src/main/java/com/notescloud/todo_service/TodoServiceApplication.java
@@ -2,12 +2,18 @@ package com.notescloud.todo_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	DataSourceAutoConfiguration.class,
+	HibernateJpaAutoConfiguration.class,
+	FlywayAutoConfiguration.class
+})
 public class TodoServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoServiceApplication.class, args);
 	}
-
 }
